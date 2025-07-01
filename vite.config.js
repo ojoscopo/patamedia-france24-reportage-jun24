6c4bd8ea4dc3ee78e5399f3d25d9379f8663cb3a
ojoscopo <ojoscopo@gmail.com>
2025-07-01 00:22:01 -0500

    Fix vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/patamedia-france24-reportage-jun24/'
+  base: './',
+  build: {
+    outDir: 'dist'
+  }
 })
